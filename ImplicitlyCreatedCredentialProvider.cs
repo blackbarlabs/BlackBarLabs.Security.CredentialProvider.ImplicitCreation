240b366b8479b35f8b68179b594a9397c8bec529
Jeremy Randolph <randocodes@gmail.com>
2016-04-13 15:10:36 -0500

    Updatig for Token Put

--- ImplicitlyCreatedCredentialProvider.cs
+++ ImplicitlyCreatedCredentialProvider.cs
@@ -45,7 +45,6 @@ namespace BlackBarLabs.Security.CredentialProvider.ImplicitCreation
 
                     // If there currently is a credential document for this providerId / username combination
                     // then check the stored password hash with the provided password hash and respond accordingly. 
-                    
                     if (String.Compare(document.AccessToken, tokenHash, false) == 0)
                         return success(tokenHash);
 
@@ -53,5 +52,56 @@ namespace BlackBarLabs.Security.CredentialProvider.ImplicitCreation
                 });
             return result;
         }
+
+
+        public async Task<TResult> UpdateTokenAsync<TResult>(Uri providerId, string username, string token,
+            Func<string, TResult> success, Func<TResult> doesNotExist, Func<TResult> updateFailed)
+        {
+            #region User MD5 hash to create a unique key for each providerId and username combination
+
+            var concatination = providerId.AbsoluteUri + username;
+            var md5 = MD5.Create();
+            byte[] md5data = md5.ComputeHash(Encoding.UTF8.GetBytes(concatination));
+            var md5guid = new Guid(md5data);
+
+            #endregion
+
+            // Create or fetch the document with that key
+
+            const string connectionStringKeyName = "Azure.Authorization.Storage";
+            var context = new Persistence.Azure.DataStores(connectionStringKeyName);
+            var result = await context.AzureStorageRepository.CreateOrUpdateAtomicAsync<TResult, CredentialsDocument>(md5guid,
+                async (document, saveDocument) =>
+                {
+                    // create hashed version of the password
+                    var tokenHashBytes = SHA512.Create().ComputeHash(Encoding.UTF8.GetBytes(token));
+                    var tokenHash = Convert.ToBase64String(tokenHashBytes);
+
+                    // If it doesn't exist, we can't update it
+                    if (default(CredentialsDocument) == document)
+                    {
+                        return doesNotExist();
+                    }
+
+                    // If there is a document for this providerId / username combination
+                    // we need up update the password for it. 
+                    //TODO: We may need to sent in the old password so that we can verify the change is valid
+                    if (default(CredentialsDocument) != document)
+                    {
+                        //TODO: Check the document.AccessToken against a passed in "OldPassword" value
+                        document.AccessToken = tokenHash;
+                        await saveDocument(document);
+                        return success(tokenHash);
+                    }
+
+                    // If they're trying to update the password with the same password then let it be success
+                    if (String.Compare(document.AccessToken, tokenHash, false) == 0)
+                        return success(tokenHash);
+
+                    return updateFailed();
+                });
+            return result;
+        }
+
     }
 }
